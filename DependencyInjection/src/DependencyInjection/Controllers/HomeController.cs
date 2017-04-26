a5c536bf908be563f891c627237c9d5a021423ac
rajachandra-thatipelli <rajachandra.thatipelli@gmail.com>
2017-04-26 17:17:03 +0530

    Add files via upload

---
+++ DependencyInjection/src/DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using DependencyInjection.Services;
+using DependencyInjection.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDBFactory _prodFactory;
+        public HomeController(IDBFactory prodFactory)
+        {
+            _prodFactory = prodFactory;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> productList = _prodFactory.GetAllProducts();
+
+            return View(productList);
+        }
+    }
+}
---
+++ DependencyInjection/src/DependencyInjection/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using DependencyInjection.Services;
+using DependencyInjection.Middleware;
+
+namespace DependencyInjection
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // Register application services.
+            services.AddScoped<IDBFactory, DBFactory>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+
+            app.UseMiddleware<AuthorizationMiddleware>();
+
+
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
+
+ 
+            // run is the end point in middleware, from here no HTTP request is proceseeded further
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
